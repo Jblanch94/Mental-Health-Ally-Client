fbb80cf856c2d544993761c157b0dde780e7865b
Johnny Blanchard <johnnyblanchard94@gmail.com>
2022-02-07 12:04:37 -0500

    add hook for tracking the width and height of a specific component.  Used by passing a ref that is the actual dom element, since I needed a child node of a component that I couldn't get access to otherwise. Used it for adjusting the width of the posts by getting the width of the sidebar component so the two could not overlap.

---
+++ src/hooks/useWindowResize.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+function useWindowResize(ref: Element | undefined) {
+  console.log(ref);
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    const updateSizeConstraints = () => {
+      setWidth(ref?.clientWidth ?? 0);
+      setHeight(ref?.clientHeight ?? 0);
+    };
+
+    updateSizeConstraints();
+    window.addEventListener("resize", updateSizeConstraints);
+    return () => window.removeEventListener("resize", updateSizeConstraints);
+  }, [ref?.clientWidth, ref?.clientHeight]);
+
+  return { height, width };
+}
+
+export default useWindowResize;
